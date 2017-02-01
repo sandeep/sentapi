303e59a36c31c0be2da4df0433f180d7a32a13b7
Sandeep Chivukula <sandeepc@yahoo.com>
2017-02-01 01:10:59 -0800

    Multiple Inputs

--- api.py
+++ api.py
@@ -3,6 +3,7 @@ import pattern;
 
 from pattern.en import sentiment
 from nltk.sentiment import SentimentIntensityAnalyzer
+from pattern.en import parse
 
 from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
@@ -11,8 +12,10 @@ from flask_restful import reqparse, abort, Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+
 parser = reqparse.RequestParser()
-parser.add_argument('text', required =True, type=str, help='Requires text attribute. Returns a polarity, subjectivity score for the given sentence, based on the adjectives it contains, where polarity is a value between -1.0 and +1.0 and subjectivity between 0.0 and 1.0. ')
+parser.add_argument('text', type=str, help='Requires text attribute. Returns a polarity, subjectivity score for the given sentence, based on the adjectives it contains, where polarity is a value between -1.0 and +1.0 and subjectivity between 0.0 and 1.0. ')
+parser.add_argument('messages', type = list, location='json')
 
 class Sentiment(Resource):
 	def get(self):
@@ -22,16 +25,26 @@ class Sentiment(Resource):
 
 	def post(self):
 		args = parser.parse_args()
-		#we should sanitize this.
-		messageSentiment = sentiment(str(args['text']))
+		output = []
+		if args['messages']:
+			messages =  args['messages']
+		else:
+				#we should sanitize this.
+			messages = [{"text": args['text'],"id": ''}]
 
-		print messageSentiment[0]
-		return {'sentiment':
-		{
+		for message in messages:
+			print message
+			messageSentiment = sentiment(str(message["text"]))
+			output.append({'sentiment':
+			{
 			'polarity': messageSentiment[0],
 			'subjectivity': messageSentiment[1]
-		}
-		}
+			},
+			'id': message["id"]
+			})
+
+
+		return {'sentiments':output}
 
 api.add_resource(Sentiment, '/')
 
